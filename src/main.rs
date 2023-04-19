9b09b1bc97f68f7e7d9897d05fb89764f48950d4
Alfredo Deza <alfredo@deza.pe>
2023-04-19 00:09:27 +0000

    add error reporting

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,8 @@
-use std::process::Command;
-
-fn run_command(command: &str) -> String {
-    let args: Vec<&str> = command.split(" ").collect();
-    let output = Command::new(args[0])
-        .args(&args[1..])
-        .output()
-        .expect("Failed to execute command");
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    stdout.to_string()
-}
-
-fn run_lsblk(device: &str) -> serde_json::Value {
-    let command = "lsblk -J -o NAME,SIZE,TYPE,MOUNTPOINT";
-    let output = run_command(command);
-    let devices: serde_json::Value = serde_json::from_str(&output).unwrap();
-    let devices = devices["blockdevices"].as_array().unwrap();
-    for parent in devices {
-        if parent["name"] == device {
-            return parent.clone();
-        }
-        if let Some(children) = parent["children"].as_array() {
-            for child in children {
-                if child["name"] == device {
-                    return child.clone();
-                }
-            }
-        }
-    }
-    panic!("Device not found");
-}
+use clap::App;
+use blkrs::run_lsblk;
 
 fn main() {
-    let matches = clap::App::new("lsblk")
+    let matches = App::new("lsblk")
         .version("0.0.1")
         .author("Alfredo Deza")
         .about("lsblk in Rust")
